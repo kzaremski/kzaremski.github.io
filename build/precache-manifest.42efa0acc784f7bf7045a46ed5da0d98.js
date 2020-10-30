a9937e3fb350ba875df946e7768c418e639b659b
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2020-10-30 00:48:07 -0600

    Build for github pages

---
+++ build/precache-manifest.42efa0acc784f7bf7045a46ed5da0d98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484c7021f33036c1e231c68397783459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63c3100070e6f36953c",
+    "url": "/static/js/2.b1ac0e69.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.b1ac0e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce28fbf4a0b13fd863a4",
+    "url": "/static/js/main.855f27fe.chunk.js"
+  },
+  {
+    "revision": "d4712a35f19fef376666",
+    "url": "/static/js/runtime-main.4ff456f3.js"
+  }
+]);
