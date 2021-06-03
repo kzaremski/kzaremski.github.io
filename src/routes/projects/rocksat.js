06ecbef230f00c03467c1d19e98610761aab4c97
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2021-06-03 00:56:56 -0600

    Added RockSat 2020 work

--- src/routes/projects/rocksat.js
+++ src/routes/projects/rocksat.js
@@ -16,47 +16,46 @@ export class Project_RockSat2020 extends React.Component {
   render() {
     return (
       <div>
-        <h1 className='px-4'><span className='h4 text-secondary'><Link to='/projects'><i className='fas fa-folder-open mr-2'></i>Projects/ </Link></span>Jolie & Co. Animated Logo</h1>
+        <h1 className='px-4'><span className='h4 text-secondary'><Link to='/projects'><i className='fas fa-folder-open mr-2'></i>Projects/ </Link></span>RockSat-X 2020</h1>
         <div className='card bg-primary shadow-soft border-light mb-4'>
           <div className='card-body'>
             <p className='card-text'>
-            I was contracted to animate the logo of Jolie & Co, a Denver based wedding stationery company and integrate it into their existing website cover page on Squarespace.
+              I was brought on in May 2021 to help with the final integration of Red Rocks Community College and Arapahoe Community College’s payload for the RockSat 2020 mission.
             </p>
           </div>
         </div>
         <div className='card bg-primary shadow-soft border-light mb-4'>
           <div className='card-body'>
-            <h4>Technical Summary</h4>
+            <h4>Mission</h4>
+            <p>The team's payload is called VRSE (Virtual Reality Space Experience), which extends an arm with a camera 16" out from the body of the rocket to record a short video at the highest point in the rocket’s orbit. The timing for extension/retraction of the arm</p>
+            <strong>Flight Timeline:</strong>
             <ul>
-              <li>An SVG approach was attempted, but I made the decision to move to an animated GIF because consistency across platforms, scaling, and screen sizes was guaranteed.</li>
-              <li>The draw on effect was achieved by animating the stroke offset of the paths within Adobe After Effects.</li>
-              <li>Any color from the stroke was a mask that would make the underlining logo calligraphy visible.</li>
-              <li>Once finished, the composition was exported as a GIF that would not loop.</li>
+              <li>30 seconds before launch (T-30s) power is applied to all payloads on the rocket. The VRSE's computer boots up and begins to listen for further signals.</li>
+              <li>85 seconds after launch (T+85s) power is applied to the first timer event line triggering arm extension and starts camera recording.</li>
+              <li>261 seconds after launch (T+261s) power is applied to the second timer event line which triggers arm retraction, stops camera recording, and transfers a low resolution copy of the camera video to the computer for redundancy.</li>
+              <li>330 seconds after launch (T+330s) power is applied to the third and final timer event line which starts the safe shutdown of the computer and all other electronic systems in preparation for re-entry.</li>
             </ul>
-            <img src='/static/img/jolieae.png' alt='' className='img-responsive rounded shadow-light'/>
-            <small>Pictured: The draw on effect consisting of multiple paths with their strokes and offsets animated within the Adobe After Effects workspace.</small>
           </div>
         </div>
         <div className='card bg-primary shadow-soft border-light mb-4'>
           <div className='card-body'>
-            <h4>Squarespace Integration</h4>
+            <h4>My Contributions</h4>
+            <p>I was recommended to the team because of my experience with Python and Linux.</p>
+            <ul>
+              <li>I refactored the existing code for camera and arm control to be more robust by adding a logging method, implementing concurrent arm extension and recording using threads, and properly handling exceptions among other improvements.</li>
+              <li>I applied my Linux system administration skills to create a systemd service unit file that would allow for the control script to start as a part of the control computer's boot process.</li>
+              <li>I incorporated the camera mounting and video transfer into the Python script and had it copy files more dynamically to account for system clock inaccuracies.</li>
+            </ul>
+            <img src='/static/img/rocksatselfie.png' alt='' className='img-responsive rounded shadow-light'/>
+            <small>The fully assembled payload and I after a successful test.</small>
+          </div>
+        </div>
+        <div className='card bg-primary shadow-soft border-light mb-4'>
+          <div className='card-body'>
+            <h4>Conclusion</h4>
             <p className='card-text'>
-              Adding the animated logo to their cover page was as simple as including the final GIF in the flow as a vertically and horizontally centered image. The client wanted the logo to be bigger, however Squarespace's editor uses a slider to control the size of the image and it was already set to the maximum size. I got around this by using Chrome Developer Tools on the Squarespace editor and manually changing the value and maximum value of the size slider to an integer beyond what was originally intended. To my surprise this was effective and the unorthodox changes were reflected on the public site. There were no issues with styling or fixes on Squarespace's end for as long as this cover page was in service.
-            </p>
-          </div>
-        </div>
-        <div className='card bg-primary shadow-soft border-light mb-4'>
-          <div className='card-body'>
-            <h4>Demo</h4>
-            <div className='embed-responsive embed-responsive-16by9'>
-              <iframe title='Jolie Cover Page' src={'/static/jolie/cover.html?a=' + Math.random()} className='rounded embed-responsive-item'></iframe>
-            </div>
-          </div>
-        </div>
-        <div className='card bg-primary shadow-soft border-light mb-4'>
-          <div className='card-body'>
-            <p className='card-text'>
-              <strong>UPDATE:</strong> As of July 10, 2020 the cover page with the animated logo is no longer in use by the company. 
+              NASA will be conducting their own tests on the physical tolerances of the payload to a variety of forces over the course of the summer (2021).
+              The RockSat 2020 mission will launch from the NASA Wallops Flight Facility, Virginia in August of 2021.
             </p>
           </div>
         </div>
